635c27365560280093875e7012365ccfc5e9cbf9
bishalale2002 <bishalalemagar12345@gmail.com>
2025-12-15 21:45:49 +0545

    Inital Debugging

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
+const connectDB = require("./config/db"); // ✅ correct
 
 const app = express();
+
+// connect database
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/appointments", require("./routes/appointmentRoutes"));
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
