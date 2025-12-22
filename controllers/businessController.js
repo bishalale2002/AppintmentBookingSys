01f160ea63c5ce1bd9cdc7e3e2c4e9486b4ff285
bishalale2002 <bishalalemagar12345@gmail.com>
2025-12-22 18:38:14 +0545

    functiopn to delete the details

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -64,3 +64,32 @@ exports.updateBusinessSchedule = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+/**
+ * @desc    Delete entire business
+ * @route   DELETE /api/business/:businessId
+ * @access  Private (Admin)
+ */
+exports.deleteBusiness = async (req, res) => {
+  try {
+    const { businessId } = req.params;
+
+    const business = await Business.findOneAndDelete({
+      _id: businessId,
+      businessOwner: req.admin._id,
+    });
+
+    if (!business) {
+      return res.status(404).json({
+        success: false,
+        message: "Business not found or unauthorized",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Business deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
