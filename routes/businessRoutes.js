6983bfc8ac43dc3721d14318f80423cc35ba8990
bishalale2002 <bishalalemagar12345@gmail.com>
2025-12-23 21:05:35 +0545

    additional functionss

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const { businessOwner } = require("../controllers/businessController");
 const adminProtect = require("../middleware/adminAuth.js");
 
-router.post("/business/register", adminProtect, businessOwner);
+router.post("/register", adminProtect, businessOwner);
+router.put(
+  "/business/:businessId/schedule",
+  adminAuth,
+  updateBusinessSchedule
+);
+
+router.delete(
+  "/business/:businessId",
+  adminAuth,
+  deleteBusiness
+);
 
 
 module.exports = router;
